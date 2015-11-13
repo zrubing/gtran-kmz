ac3e61ca3dd42e8bf25a27390d9e0b7856e5c103
haoliangyu <njkon@foxmail.com>
2015-11-13 00:24:01 -0500

    migrate codes from main project

---
+++ src/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var JSZip = require('jszip');
+var kml = require('gtran-kml');
+var promiseLib = require('./promise.js');
+
+var Promise, writeFile;
+
+exports.setPromiseLib = setPromiseLib;
+
+exports.fromGeoJson = function(geojson, fileName, options) {
+    if (!Promise) { setPromiseLib(); }
+
+    return kml.fromGeoJson(geojson).then(function(file) {
+        var zip = new JSZip();
+        zip.file('doc.kml', file.data);
+
+        var buffer = zip.generate({type:"nodebuffer"});
+        if(fileName) {
+            var fileNameWithExt = fileName;
+            if(fileNameWithExt.indexOf('.kmz') === -1) {
+                fileNameWithExt += '.kmz';
+            }
+
+            writeFile(fileNameWithExt, buffer);
+
+            return Promise.resolve(fileNameWithExt);
+        } else {
+            return Promise.resolve({
+                data: buffer,
+                format: 'kmz'
+            });
+        }
+    })
+};
+
+function setPromiseLib(lib) {
+    Promise = promiseLib.set(lib);
+    writeFile = promiseLib.promisify(require('fs').writeFile);
+
+    kml.setPromiseLib(lib);
+}
